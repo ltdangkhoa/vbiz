d4efcc12a6d06732442a57b1ee7cb47f8a03a508
Kris Luu <krisluu@Kriss-MacBook-Air.local>
2019-04-21 22:36:23 +0700

    Updates...

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='vbiz',
-    version='0.0.1',
+    version='0.0.7',
     author='Kris Luu',
     author_email='luuthaidangkhoa@gmail.com',
     description='The vbiz fetcher',
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url='http://github.com/ltdangkhoa/vbiz',
+    url='https://github.com/ltdangkhoa/vbiz',
     packages=setuptools.find_packages(),
     install_requires=[
         'pdfminer.six',
