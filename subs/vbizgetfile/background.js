0365cd119183da57dcb201203f7881dc76ccf401
Kris <luuthaidangkhoa@gmail.com>
2019-05-02 18:34:07 +0700

    Updates...

--- subs/vbizgetfile/background.js
+++ subs/vbizgetfile/background.js
@@ -1,16 +1,25 @@
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {command: "click"}, function(response) {
-          autovbiz = !autovbiz;
-        });
-    });
+  autovbiz = !autovbiz;
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      command: "click"
+    }, function(response) {
 
+    });
+  });
 });
+
 var autovbiz = false;
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  console.log('autovbiz' + autovbiz);
   if (changeInfo.status == 'complete') {
-    if(tab.url.includes('https://bocaodientu.dkkd.gov.vn') && autovbiz){
-      chrome.tabs.sendMessage(tabId, {command: "auto"}, function(response) {
+    if (tab.url.includes('https://bocaodientu.dkkd.gov.vn') && autovbiz) {
+      chrome.tabs.sendMessage(tabId, {
+        command: "auto"
+      }, function(response) {
         console.log("Hey hey auto!")
       });
     }
